90acc79b0592d8ab42cf15d35b640e5fca7a3481
lionika88@mail.ru <saulespuke101@gmail.com>
2017-09-27 14:08:36 +0300

    20170920_14_10

---
+++ shell_examples.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#3. piemērs
+NAME="Vārds Uzvārds"
+echo $NAME
+unset NAME
+echo $NAME
+
+#2. piemērs
+#NAME="Vārds Uzvārds"
+#readonly NAME
+#echo $NAME
+#NAME="Uzvārds Vārds"
+#echo $Name
+
+#1.piemērs
+#NAME="Vārds Uzvārds"
+#echo $NAME
+
+#0. piemērs
+#history > history_20170927.txt
+
