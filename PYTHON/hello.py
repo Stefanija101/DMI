d12f3e96edd4d50c73243f832e0f720f1699452f
rastamaha1998@gmail.com <saulespuke101@gmail.com>
2017-11-08 15:59:54 +0200

    20171108_16_00

---
+++ PYTHON/hello.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python 
+# -*- coding: utf-8 -*-
+
+# example dzejolis
+#echo -e 'Ārā \n\tklusām \vlien \vlietus, \vun \vmākoņi \vskaļi \velpo
+#echo
+#kaut \vkur \vbezgalībā \vsteidz \vnokļūt \vpeļķes.'
+print 'Ārā \n\tvklusām \vlien \vlietus, \vun \vmākoņi \vskaļi \velpo \b\b\b\b\b\bbkur \vkaut \vkur \b\b\b\b\b\b\b\bbezgalībā \bsteidz \bnokļūt \bpeļķes.'
+
+
+'''
+# example 7
+#echo -e 'Here the \rcontent before this is removed.'  
+print 'Here the \rcontent before this is removed.'  
+'''
+
+'''
+# example 6
+# echo -e 'Here \vthe \vspaces \vhave \vvertical \vtab \vspaces.'
+print 'Here \vthe \vspaces \vhave \vvertical \vtab \vspaces.'
+'''
+
+'''
+# example 5
+#echo -e 'Here \tthe \tspaces \thave \thorizontal \ttab \tspaces.'
+print 'Here \tthe \tspaces \thave \thorizontal \ttab \tspaces.'
+'''
+
+'''
+# example 4
+# echo -e 'Here \nthe \nspaces \nare \nnewlined.'
+print 'Here \nthe \nspaces \nare \nnewlined.'
+'''
+
+'''
+# example 3 
+# echo -e 'Here \bthe \bspaces \bare \bbackspaced.'
+print "Here \bthe \bspace \bare \bbackspaced."
+'''
+
+'''
+# 1. pirmais piemērs
+print "Hello World!"
+'''
---
+++ PYTHON/while.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+a = input("LIetotāj, lūdzu, ievadi beigu vērtību: ")
+
+k = 1
+while k <= a:
+    print "Mainīgā k kārtējā vērtība ir: %d\n"%(k),
+    k = k + 1
+print "Beigas!"
+    
